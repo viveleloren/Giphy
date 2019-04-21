8d40e5e45f83ca55b709e4d96967437ecb44f060
Loren Brown <viveleloren@gmail.com>
2019-04-20 23:24:50 -0700

    Final Push

---
+++ assets/javascript/app.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    //Array for searched topics to be added
+    var topics = [];
+
+    //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
+    //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+    function displayCaffeine() {
+
+        var x = $(this).data("search");
+        console.log(x);
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            var results = response.data;
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+
+                var showDiv = $("<div class='col-md-4'>");
+
+                var rating = results[i].rating;
+                var defaultAnimatedSrc = results[i].images.fixed_height.url;
+                var staticSrc = results[i].images.fixed_height_still.url;
+                var showImage = $("<img>");
+                var p = $("<p>").text("Rating: " + rating);
+
+                showImage.attr("src", staticSrc);
+                showImage.addClass("caffeineGiphy");
+                showImage.attr("data-state", "still");
+                showImage.attr("data-still", staticSrc);
+                showImage.attr("data-animate", defaultAnimatedSrc);
+                showDiv.append(p);
+                showDiv.append(showImage);
+                $("#gifArea").prepend(showDiv);
+
+            }
+        });
+    }
+
+    //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+    $("#addCaffeine").on("click", function (event) {
+        event.preventDefault();
+        var newCaffeine = $("#caffeineInput").val().trim();
+        topics.push(newCaffeine);
+        console.log(topics);
+        $("#caffeineInput").val('');
+        displayButtons();
+    });
+
+    //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+    function displayButtons() {
+        $("#myButtons").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $('<button class="btn btn-primary">');
+            a.attr("id", "show");
+            a.attr("data-search", topics[i]);
+            a.text(topics[i]);
+            $("#myButtons").append(a);
+        }
+    }
+
+
+    displayButtons();
+
+    //Click event on button with id of "show" executes displayCaffeine function
+    $(document).on("click", "#show", displayCaffeine);
+
+    //Click event on gifs with class of "caffeineGiphy" executes pausePlayGifs function
+    $(document).on("click", ".caffeineGiphy", pausePlayGifs);
+
+    //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+    function pausePlayGifs() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
+
+});
